522d0de4b3f7d45ab515bb2b79272dcabb2e9770
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-03-22 07:01:14 +0100

    implemented auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import { FunctionComponent } from "react";
 import Button from "@/component/ui/Button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 interface PageProps {}
- 
-const Page: FunctionComponent<PageProps> = () => {
-    return <Button isLoading={false}>Hello</Button>;
-}
- 
+
+const Page: FunctionComponent<PageProps> = async () => {
+  const session = await getServerSession(authOptions);
+
+  return <pre>{JSON.stringify(session)}</pre>;
+};
+
 export default Page;
