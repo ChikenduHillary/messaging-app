5b804d83dcd0a857228d9c0f2b8c6a31c254521f
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-03-22 06:33:52 +0100

    update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,14 @@
 import { Redis } from "@upstash/redis";
 
+// Asserting the types of environment variables and providing defaults
+const redisUrl: string | undefined = process.env.UPSTASH_REDIS_REST_URL;
+const redisToken: string | undefined = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+if (!redisUrl || !redisToken) {
+  throw new Error("Redis URL or token is not provided.");
+}
+
 export const db = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: redisUrl,
+  token: redisToken,
 });
