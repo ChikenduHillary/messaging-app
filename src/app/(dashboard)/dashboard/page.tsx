5f6c7aa43e2f33578522812f709ac298a3fcb9bd
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-05-01 15:21:02 +0100

    fixed bug

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { FunctionComponent } from "react";
-import Button from "@/component/ui/Button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-interface PageProps {}
-
-const Page: FunctionComponent<PageProps> = async () => {
+const Page = async () => {
   const session = await getServerSession(authOptions);
 
   return <pre>{JSON.stringify(session)}</pre>;
